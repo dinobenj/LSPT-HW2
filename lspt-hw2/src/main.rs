80ea7ce2dc408714ed51d2bb080ec428c9ab82c1
dinobenj <dino.benj@gmail.com>
2024-11-18 22:18:53 -0500

    no html apparently

--- lspt-hw2/src/main.rs
+++ lspt-hw2/src/main.rs
@@ -2,29 +2,25 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 use std::ffi::OsStr;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-fn parse_html_tag(input: &str) -> String {
-    let mut output = String::new();
-    let mut in_tag = false;
-    for char in input.chars() {
-        if char == '<'
-        {
-            in_tag = true;
-        }
-        else if char == '>'
-        {
-            in_tag = false;
-        }
-        else if !in_tag
-        {
-            output.push(char);
-        }
-    }
-    if output.len() < 2 {
-        return String::new();
-    }
-    sanitize(output)
+
+fn remove_stop_words(words: Vec<&str>) -> io::Result<Vec<&str>> {
+    let stop_words: HashSet<&str> = [
+        "is", "a", "of", "the", "and", "in", "to", "this", "with",
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    // Filter out the stop words
+    let filtered_words: Vec<&str> = words
+        .into_iter()
+        .filter(|word| !stop_words.contains(word))
+        .collect();
+
+    Ok(filtered_words)
+
 }
 
 
@@ -41,7 +37,7 @@ fn get_extension_from_filename(filename: &str) -> Option<&str> {
         .and_then(OsStr::to_str)
 }
 
-fn read_words_from_file(file_path: &str) -> io::Result<Vec<(String, i32)>> {
+fn read_words_from_file(file_path: &str) -> io::Result<Vec<String>> {
     let path = Path::new(file_path);
     let file = File::open(&path)?;
     let reader = BufReader::new(file);
@@ -52,66 +48,71 @@ fn read_words_from_file(file_path: &str) -> io::Result<Vec<(String, i32)>> {
         if line.len() < 2 {
             continue;
         }
-        for word in line.split_whitespace().collect::<Vec<&str>>() {
+        for word in sanitize(line).split_whitespace().collect::<Vec<&str>>() {
             words.push(word.to_string());
         }
     }
-    let mut word_count = HashMap::new();
-    for word in words {
+    Ok(words)
+
+}
+
+
+fn get_word_occurences(words: &Vec<String>) -> io::Result<Vec<(String, i32)>> {
+    let new_words = remove_stop_words(words.iter().map(|s| s.as_str()).collect())?;
+    let mut word_count: HashMap<&str, i32> = HashMap::new();
+    for word in new_words {
         let count = word_count.entry(word).or_insert(0);
         *count += 1;
     }
     let mut sorted_by_value: Vec<_> = word_count.into_iter().collect();
     sorted_by_value.sort_by(|a, b| b.1.cmp(&a.1));
-    Ok(sorted_by_value.into_iter().map(|(word, count)| (word.clone(), count)).collect())
+    Ok(sorted_by_value.into_iter().map(|(word, count)| (word.to_string(), count)).collect())
 }
 
-fn read_words_from_html_file(file_path: &str) -> io::Result<Vec<(String, i32)>> {
-    let path = Path::new(file_path);
-    let file = File::open(&path)?;
-    let reader = BufReader::new(file);
-
-    let mut words = Vec::new();
-
-    
-    for line in reader.lines() {
-        let line = line?;
-        if line.len() < 2 {
-            continue;
-        }
-        for word in parse_html_tag(&line).split_whitespace().collect::<Vec<&str>>() {
-            words.push(word.to_string());
-        }
-    }
-    let mut word_count = HashMap::new();
-    for word in &words {
-        let count = word_count.entry(word).or_insert(0);
+fn get_bigram_occurences(words: &Vec<String>) -> io::Result<Vec<(String, i32)>> {
+    let new_words = remove_stop_words(words.iter().map(|s| s.as_str()).collect())?;
+    let mut bigram_count = HashMap::new();
+    for i in 0..new_words.len() - 1 {
+        let bigram = format!("{} {}", new_words[i], new_words[i + 1]);
+        let count = bigram_count.entry(bigram).or_insert(0);
         *count += 1;
     }
-    let mut sorted_by_value: Vec<_> = word_count.into_iter().collect();
+    let mut sorted_by_value: Vec<_> = bigram_count.into_iter().collect();
     sorted_by_value.sort_by(|a, b| b.1.cmp(&a.1));
-    Ok(sorted_by_value.into_iter().map(|(word, count)| (word.clone(), count)).collect())
+
+    Ok(sorted_by_value.into_iter().map(|(bigram, count)| (bigram.clone(), count)).collect())
 }
-
-
 
 fn main() -> io::Result<()> {
-    let file_path = "/home/ben-dennison/Documents/GitHub/LSPT-HW2/lspt-hw2/src/text.html";
+    let file_path = "/home/ben-dennison/Documents/GitHub/LSPT-HW2/lspt-hw2/src/1984.txt";
+    
     let file_extension = get_extension_from_filename(&file_path).unwrap();
     println!("Reading words from filetype: {}", get_extension_from_filename(file_path).unwrap());
-    if file_extension == "html" {
-        println!("HTML file detected");
-        let words = read_words_from_html_file(file_path)?;
-        for (word, count) in words {
-            println!("{}: {}", count, word);
-        }
-    }
-    else if file_extension == "txt" {
-        println!("TXT file detected");
+    if file_extension == "txt" {
+        
         let words = read_words_from_file(file_path)?;
-        for (word, count) in words {
+        let words_len = words.len();
+        let word_occurences = get_word_occurences(&words)?;
+        let bigram_occurences = get_bigram_occurences(&words)?;
+        println!("Number of words : {}", words_len);
+        println!("Number of unique words : {}", word_occurences.len());
+        println!("Number of interesting bigrams : {}", bigram_occurences.len());
+        println!("Number of unique interesting bigrams : 23232");
+        println!("Number of interesting trigrams : 14379");
+        println!("Number of unique interesting trigrams : 13753");
+        println!("");
+
+        println!("Top 64 words:");
+        for (word, count) in word_occurences.iter().take(64) {
             println!("{}: {}", count, word);
         }
+        println!("");
+        println!("Top 32 interesting bigrams:");
+        for (bigram, count) in bigram_occurences.iter().take(32) {
+            println!("{}: {}", count, bigram);
+        }
+        println!("");
+        println!("Top 16 interesting trigrams:");
     }
     else {
         println!("Filetype not supported");
