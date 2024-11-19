9965d90ac621dedc14fb351e1c8c512ac2f9da31
dinobenj <dino.benj@gmail.com>
2024-11-19 00:08:15 -0500

    evil ahh list

--- lspt-hw2/src/main.rs
+++ lspt-hw2/src/main.rs
@@ -7,7 +7,12 @@ use std::collections::{HashMap, HashSet};
 
 fn remove_stop_words(words: Vec<&str>) -> io::Result<Vec<&str>> {
     let stop_words: HashSet<&str> = [
-        "is", "a", "of", "the", "and", "in", "to", "this", "with",
+        "the", "of", "to", "a", "and", "in", "said", "for", "that", "was", 
+        "on", "he", "is", "with", "at", "by", "it", "from", "as", "be", 
+        "were", "an", "have", "his", "but", "has", "are", "not", "who", 
+        "they", "its", "had", "will", "would", "about", "i", "been", 
+        "this", "their", "new", "or", "which", "we", "more", "after", 
+        "us", "percent", "up", "one", "people",
     ]
     .iter()
     .cloned()
@@ -24,11 +29,11 @@ fn remove_stop_words(words: Vec<&str>) -> io::Result<Vec<&str>> {
 }
 
 
-fn sanitize(check: String) -> String {
+fn clean(check: String) -> String {
     let temp: String = check.chars()
-        .filter(|&c| c != '\n' && c != '\t' && c != '\r')
+        .filter(|&c| c != '\n' && c != '\t' && c != '\r' && c != '\'')
         .collect();
-    temp.replace(|c| !char::is_alphanumeric(c), " ")
+    temp.replace(|c| !char::is_alphanumeric(c), " ").to_lowercase()
 }
 
 fn get_extension_from_filename(filename: &str) -> Option<&str> {
@@ -48,14 +53,13 @@ fn read_words_from_file(file_path: &str) -> io::Result<Vec<String>> {
         if line.len() < 2 {
             continue;
         }
-        for word in sanitize(line).split_whitespace().collect::<Vec<&str>>() {
+        for word in clean(line).split_whitespace().collect::<Vec<&str>>() {
             words.push(word.to_string());
         }
     }
     Ok(words)
 
 }
-
 
 fn get_word_occurences(words: &Vec<String>) -> io::Result<Vec<(String, i32)>> {
     let new_words = remove_stop_words(words.iter().map(|s| s.as_str()).collect())?;
@@ -83,6 +87,20 @@ fn get_bigram_occurences(words: &Vec<String>) -> io::Result<Vec<(String, i32)>> 
     Ok(sorted_by_value.into_iter().map(|(bigram, count)| (bigram.clone(), count)).collect())
 }
 
+fn get_trigram_occurences(words: &Vec<String>) -> io::Result<Vec<(String, i32)>> {
+    let new_words = remove_stop_words(words.iter().map(|s| s.as_str()).collect())?;
+    let mut bigram_count = HashMap::new();
+    for i in 0..new_words.len() - 2 {
+        let bigram = format!("{} {} {}", new_words[i], new_words[i + 1], new_words[i + 2]);
+        let count = bigram_count.entry(bigram).or_insert(0);
+        *count += 1;
+    }
+    let mut sorted_by_value: Vec<_> = bigram_count.into_iter().collect();
+    sorted_by_value.sort_by(|a, b| b.1.cmp(&a.1));
+
+    Ok(sorted_by_value.into_iter().map(|(bigram, count)| (bigram.clone(), count)).collect())
+}
+
 fn main() -> io::Result<()> {
     let file_path = "/home/ben-dennison/Documents/GitHub/LSPT-HW2/lspt-hw2/src/1984.txt";
     
@@ -94,12 +112,13 @@ fn main() -> io::Result<()> {
         let words_len = words.len();
         let word_occurences = get_word_occurences(&words)?;
         let bigram_occurences = get_bigram_occurences(&words)?;
+        let trigram_occurences = get_trigram_occurences(&words)?;
         println!("Number of words : {}", words_len);
         println!("Number of unique words : {}", word_occurences.len());
         println!("Number of interesting bigrams : {}", bigram_occurences.len());
         println!("Number of unique interesting bigrams : 23232");
         println!("Number of interesting trigrams : 14379");
-        println!("Number of unique interesting trigrams : 13753");
+        println!("Number of unique interesting trigrams : {}", trigram_occurences.len());
         println!("");
 
         println!("Top 64 words:");
@@ -113,6 +132,9 @@ fn main() -> io::Result<()> {
         }
         println!("");
         println!("Top 16 interesting trigrams:");
+        for(trigram, count) in trigram_occurences.iter().take(16) {
+            println!("{}: {}", count, trigram);
+        }
     }
     else {
         println!("Filetype not supported");
