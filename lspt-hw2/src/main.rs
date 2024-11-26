5bad9db3ffe419170e0a4041ff522dd81d4e7214
rspacerr <definitelynotoberon@gmail.com>
2024-11-26 01:53:12 -0500

    no stop words in ngrams

--- lspt-hw2/src/main.rs
+++ lspt-hw2/src/main.rs
@@ -2,31 +2,17 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 use std::ffi::OsStr;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::env;
 
-
-fn remove_stop_words(words: Vec<&str>) -> io::Result<Vec<&str>> {
-    let stop_words: HashSet<&str> = [
-        "the", "of", "to", "a", "and", "in", "said", "for", "that", "was", 
-        "on", "he", "is", "with", "at", "by", "it", "from", "as", "be", 
-        "were", "an", "have", "his", "but", "has", "are", "not", "who", 
-        "they", "its", "had", "will", "would", "about", "i", "been", 
-        "this", "their", "new", "or", "which", "we", "more", "after", 
-        "us", "percent", "up", "one", "people",
-    ]
-    .iter()
-    .cloned()
-    .collect();
-
-    // Filter out the stop words
-    let filtered_words: Vec<&str> = words
-        .into_iter()
-        .filter(|word| !stop_words.contains(word))
-        .collect();
-
-    Ok(filtered_words)
-}
+const STOP_WORDS: &'static[&'static str] = &[
+    "the", "of", "to", "a", "and", "in", "said", "for", "that", "was", 
+    "on", "he", "is", "with", "at", "by", "it", "from", "as", "be", 
+    "were", "an", "have", "his", "but", "has", "are", "not", "who", 
+    "they", "its", "had", "will", "would", "about", "i", "been", 
+    "this", "their", "new", "or", "which", "we", "more", "after", 
+    "us", "percent", "up", "one", "people",
+];
 
 
 /**
@@ -82,7 +68,7 @@ fn read_words_from_file(file_path: &str) -> io::Result<Vec<String>> {
     let reader = BufReader::new(file);
     let mut words = Vec::new();
 
-    // Process the text to contain only lowercase.
+    // Process each word, line by line, then add to word list.
     for line in reader.lines() {
         let line = line?;
         for word in clean(line).split_whitespace().collect::<Vec<&str>>() {
@@ -97,10 +83,21 @@ fn read_words_from_file(file_path: &str) -> io::Result<Vec<String>> {
 }
 
 fn get_bigram_occurrences(words: &Vec<&String>) -> io::Result<Vec<(String, i32)>> {
-    let new_words = remove_stop_words(words.iter().map(|s| s.as_str()).collect())?;
     let mut bigram_count = HashMap::new();
-    for i in 0..new_words.len() - 1 {
-        let bigram = format!("{} {}", new_words[i], new_words[i + 1]);
+    for i in 0..words.len() - 1 {
+        let bigram = format!("{} {}", words[i], words[i + 1]);
+
+        let mut bad: bool = false;
+        for word in STOP_WORDS {
+            if word == words[i] || word == words[i+1] {
+                bad = true;
+                break;
+            }
+        }
+        if bad {
+            continue;
+        }
+
         let count = bigram_count.entry(bigram).or_insert(0);
         *count += 1;
     }
@@ -110,14 +107,23 @@ fn get_bigram_occurrences(words: &Vec<&String>) -> io::Result<Vec<(String, i32)>
 }
 
 fn get_trigram_occurrences(words: &Vec<&String>) -> io::Result<Vec<(String, i32)>> {
-    let new_words = remove_stop_words(words.iter().map(|s| s.as_str()).collect())?;
-    let mut bigram_count = HashMap::new();
-    for i in 0..new_words.len() - 2 {
-        let bigram = format!("{} {} {}", new_words[i], new_words[i + 1], new_words[i + 2]);
-        let count = bigram_count.entry(bigram).or_insert(0);
+    let mut trigram_count = HashMap::new();
+    for i in 0..words.len() - 2 {
+        let trigram = format!("{} {} {}", words[i], words[i + 1], words[i + 2]);
+        let mut bad: bool = false;
+        for word in STOP_WORDS {
+            if word == words[i] || word == words[i+1] || word == words[i+2] {
+                bad = true;
+                break;
+            }
+        }
+        if bad {
+            continue;
+        }
+        let count = trigram_count.entry(trigram).or_insert(0);
         *count += 1;
     }
-    let mut sorted_by_value: Vec<_> = bigram_count.into_iter().collect();
+    let mut sorted_by_value: Vec<_> = trigram_count.into_iter().collect();
     sorted_by_value.sort_by(|a, b| b.1.cmp(&a.1).then_with(|| a.0.cmp(&b.0)));
     Ok(sorted_by_value.into_iter().map(|(bigram, count)| (bigram.clone(), count)).collect())
 }
